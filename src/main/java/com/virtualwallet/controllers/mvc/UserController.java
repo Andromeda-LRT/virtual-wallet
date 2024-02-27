0c7df0a66f984c1c99bf65461f72222bf319596d
LyubimaMalkocheva <lyubima.malkocheva@gmail.com>
2024-02-27 18:24:03 +0200

    Create and Implement UserController for MVC

---
+++ src/main/java/com/virtualwallet/controllers/mvc/UserController.java
@@ -0,0 +1,330 @@
+package com.virtualwallet.controllers.mvc;
+
+import com.virtualwallet.exceptions.AuthenticationFailureException;
+import com.virtualwallet.exceptions.EntityNotFoundException;
+import com.virtualwallet.exceptions.ExpiredCardException;
+import com.virtualwallet.exceptions.UnauthorizedOperationException;
+import com.virtualwallet.model_helpers.AuthenticationHelper;
+import com.virtualwallet.model_helpers.UserModelFilterOptions;
+import com.virtualwallet.model_mappers.CardMapper;
+import com.virtualwallet.model_mappers.UserMapper;
+import com.virtualwallet.models.Card;
+import com.virtualwallet.models.User;
+import com.virtualwallet.models.model_dto.CardDto;
+import com.virtualwallet.models.model_dto.UserDto;
+import com.virtualwallet.services.contracts.CardService;
+import com.virtualwallet.services.contracts.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final CardService cardService;
+    private final AuthenticationHelper authenticationHelper;
+
+    private final UserMapper userMapper;
+
+    private final CardMapper cardMapper;
+
+    @Autowired
+    public UserController(UserService userService,
+                          CardService cardService,
+                          AuthenticationHelper authenticationHelper,
+                          UserMapper userMapper, CardMapper cardMapper) {
+        this.userService = userService;
+        this.cardService = cardService;
+        this.authenticationHelper = authenticationHelper;
+        this.userMapper = userMapper;
+        this.cardMapper = cardMapper;
+    }
+
+    @ModelAttribute("isAuthenticated")
+    public boolean populateIsAuthenticated(HttpSession session) {
+        return session.getAttribute("currentUser") != null;
+    }
+
+    @GetMapping("/{id}")
+    public String showCurrentUserProfile(@PathVariable int id, Model model, HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        try {
+            User user = userService.get(id, loggedUser);
+            UserDto userDto = userMapper.toDto(user);
+            model.addAttribute("user", userDto);
+            model.addAttribute("cards", cardService.getAllUserCards(loggedUser));
+            return "ProfileView";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+
+    @PostMapping("/{id}")
+    public String updateUserProfile(@PathVariable int id,
+                                    @ModelAttribute("user") UserDto userDto,
+                                    BindingResult bindingResult,
+                                    Model model,
+                                    HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "ProfileView";
+        }
+
+        try {
+            User user = userMapper.fromDto(id, userDto);
+            userService.update(user, loggedUser);
+            return "redirect:/users/" + id;
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteUserProfile(@PathVariable int id, Model model, HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        try {
+            userService.delete(id, loggedUser);
+            return "redirect:/auth/logout";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+
+    @GetMapping("/{id}/cards")
+    public String showUserCards(@PathVariable int id, Model model, HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        try {
+            List<Card> cards = cardService.getAllUserCards(loggedUser);
+            model.addAttribute("cards", cards);
+            return "UserCardsView";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+
+    @GetMapping("/{id}/cards/addition")
+    public String showAddCardPage(@PathVariable int id, Model model, HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        model.addAttribute("card", new CardDto());
+        return "AddNewCardView";
+    }
+
+
+    @PostMapping("/{id}/cards/addition")
+    public String addCard(@PathVariable int id,
+                          @ModelAttribute("card") CardDto cardDto,
+                          BindingResult bindingResult,
+                          Model model,
+                          HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "AddNewCardView";
+        }
+
+        try {
+            Card card = cardMapper.fromDto(cardDto);
+            cardService.createCard(loggedUser, card);
+            return "redirect:/users/" + id + "/cards/addition";
+        } catch (ExpiredCardException e) {
+            model.addAttribute("statusCode", HttpStatus.BAD_REQUEST.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "AddNewCardView";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+
+    @GetMapping("/{id}/cards/{cardId}")
+    public String showCardDetails(@PathVariable int id, @PathVariable int cardId, Model model, HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        try {
+            Card card = cardService.getCard(cardId, loggedUser);
+            model.addAttribute("card", card);
+            return "CardDetailsView";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+
+    @PostMapping("/{id}/cards/{cardId}")
+    public String updateCard(@PathVariable int id,
+                             @PathVariable int cardId,
+                             @ModelAttribute("card") CardDto cardDto,
+                             BindingResult bindingResult,
+                             Model model,
+                             HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "CardDetailsView";
+        }
+
+        try {
+            Card card = cardMapper.fromDto(cardDto, cardId);
+            cardService.updateCard(card, loggedUser);
+            return "redirect:/users/" + id + "/cards/" + cardId;
+        } catch (ExpiredCardException e) {
+            model.addAttribute("statusCode", HttpStatus.BAD_REQUEST.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "AddNewCardView";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+
+
+    @GetMapping("/{id}/cards/{cardId}/deletion")
+    public String deleteCard(@PathVariable int id, @PathVariable int cardId, Model model, HttpSession session) {
+        User loggedUser;
+        try {
+            loggedUser = authenticationHelper.tryGetUser(session);
+        } catch (AuthenticationFailureException e) {
+            return "redirect:/auth/login";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        }
+
+        try {
+            cardService.deleteCard(cardId, loggedUser);
+            return "redirect:/users/" + id + "/cards";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (UnauthorizedOperationException e) {
+            model.addAttribute("statusCode", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "UnauthorizedView";
+        }
+    }
+}
