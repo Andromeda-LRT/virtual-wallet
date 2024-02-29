bb3e0009b7955f694a96f393d91dce07bed8d16f
TeodorK0907 <ted_kehayov@abv.bg>
2024-03-01 00:56:48 +0200

    Update ModelConstantHelper

--- src/main/java/com/virtualwallet/model_helpers/ModelConstantHelper.java
+++ src/main/java/com/virtualwallet/model_helpers/ModelConstantHelper.java
@@ -15,5 +15,16 @@ public class ModelConstantHelper {
     public static final String NAME_ERROR_MESSAGE = "Field should be between 3 and 20 symbols.";
     public static final String UNAUTHORIZED_OPERATION_ERROR_MESSAGE = "You are not authorized to perform this operation";
 
+    public static final String INSUFFICIENT_FUNDS_ERROR_MESSAGE = "%s, with iban %s requires %.2f" +
+            " more funds to complete the transaction.";
+
+    public static final String INVALID_AUTHENTICATION =
+            "Invalid authentication";
+    public static final String WRONG_USERNAME_OR_PASSWORD = "Wrong username or password.";
+
+    public static final String THE_REQUESTED_RESOURCE_REQUIRES_AUTHENTICATION =
+            "The requested resource requires authentication.";
+
+    public static final String DUMMY_API_BASE_URL = "http://localhost:8081";
 
 }
