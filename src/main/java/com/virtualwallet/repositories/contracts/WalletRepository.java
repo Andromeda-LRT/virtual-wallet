2c05839ed66f5bb550e09fe9f215825727aea454
LyubimaMalkocheva <lyubima.malkocheva@gmail.com>
2024-03-04 17:54:42 +0200

    Develop Wallet repository

--- src/main/java/com/virtualwallet/repositories/contracts/WalletRepository.java
+++ src/main/java/com/virtualwallet/repositories/contracts/WalletRepository.java
@@ -1,4 +1,22 @@
 package com.virtualwallet.repositories.contracts;
 
+
+import com.virtualwallet.models.Card;
+import com.virtualwallet.models.User;
+import com.virtualwallet.models.Wallet;
+import com.virtualwallet.models.WalletToWalletTransaction;
+
+import java.util.List;
+
 public interface WalletRepository {
+    List<Wallet> getAll();
+    Wallet getByStringField(String fieldName, String fieldValue);
+    Wallet getById(int id);
+    void create(Wallet wallet);
+    void update(Wallet wallet);
+    void delete(int id);
+
+    List<Wallet> getAllWallets(User user);
+
+    boolean checkWalletOwnership(int userId, int walletId);
 }
