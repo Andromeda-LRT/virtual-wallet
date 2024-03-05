95559ec347890b8e6b66f45d981818607ece7dd7
reni <reneta.marinova97@gmail.com>
2024-03-05 10:57:02 +0200

    WalletToWalletTransactionService Repository implementation

---
+++ src/main/java/com/virtualwallet/repositories/contracts/WalletToWalletTransactionRepository.java
@@ -0,0 +1,24 @@
+package com.virtualwallet.repositories.contracts;
+
+import com.virtualwallet.model_helpers.TransactionModelFilterOptions;
+import com.virtualwallet.models.User;
+import com.virtualwallet.models.Wallet;
+import com.virtualwallet.models.WalletToWalletTransaction;
+import jakarta.transaction.Transaction;
+
+import java.util.List;
+
+public interface WalletToWalletTransactionRepository {
+    List<WalletToWalletTransaction> getAll();
+
+    List<WalletToWalletTransaction> getAllWalletTransactionsWithFilter
+            (User user, TransactionModelFilterOptions transactionFilter);
+
+    List<WalletToWalletTransaction> getUserWalletTransactions(Wallet wallet);
+
+    WalletToWalletTransaction getById(int walletTransactionId);
+
+    void create(WalletToWalletTransaction transaction);
+
+    void update(WalletToWalletTransaction transaction);
+}
