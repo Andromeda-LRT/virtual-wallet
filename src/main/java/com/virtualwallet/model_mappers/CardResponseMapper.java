460623b229ccef161ce24398d59aa9268c5de91d
TeodorK0907 <ted_kehayov@abv.bg>
2024-03-16 22:02:06 +0200

    Update CardResponseMapper and WalletMapper

--- src/main/java/com/virtualwallet/model_mappers/CardResponseMapper.java
+++ src/main/java/com/virtualwallet/model_mappers/CardResponseMapper.java
@@ -7,15 +7,21 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.virtualwallet.model_helpers.ModelConstantHelper.CARD_LAST_4_DIGIT_START_INDEX;
+import static com.virtualwallet.model_helpers.ModelConstantHelper.HIDDEN_CARD_DIGITS;
+
 @Component
 public class CardResponseMapper {
+
+
+
     public CardResponseMapper() {
     }
 
     public CardResponseDto toResponseDto(Card card) {
         CardResponseDto cardResponseDto = new CardResponseDto();
         cardResponseDto.setId(card.getId());
-        cardResponseDto.setNumber(card.getNumber());
+        cardResponseDto.setNumber(HIDDEN_CARD_DIGITS + card.getNumber().substring(CARD_LAST_4_DIGIT_START_INDEX));
         cardResponseDto.setCardHolder(card.getCardHolder());
         cardResponseDto.setExpirationDate(card.getExpirationDate());
         cardResponseDto.setCheckNumber(card.getCheckNumber().getCvv());
