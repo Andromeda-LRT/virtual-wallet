c0fc61fd4d3d2f2a3fea5a683735cbc2ad22f657
TeodorK0907 <ted_kehayov@abv.bg>
2024-03-16 22:03:52 +0200

    Update LoginDto

--- src/main/java/com/virtualwallet/models/mvc_input_model_dto/LoginDto.java
+++ src/main/java/com/virtualwallet/models/mvc_input_model_dto/LoginDto.java
@@ -1,23 +1,13 @@
 package com.virtualwallet.models.mvc_input_model_dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 
-import static com.virtualwallet.model_helpers.ModelConstantHelper.PASSWORD_ERROR_MESSAGE;
-import static com.virtualwallet.model_helpers.ModelConstantHelper.USERNAME_ERROR_MESSAGE;
+public class LoginDto extends WalletUserDto{
 
-public class LoginDto {
-    @NotEmpty(message = "Username can't be empty.")
-    private String username;
     @NotEmpty(message = "Password can't be empty.")
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+    public LoginDto() {
     }
 
     public String getPassword() {
