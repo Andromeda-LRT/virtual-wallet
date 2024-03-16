733698f76b7891058319488b56f7679cb1eb58f9
LyubimaMalkocheva <lyubima.malkocheva@gmail.com>
2024-03-16 22:50:02 +0200

    Fix validation

--- src/main/java/com/virtualwallet/models/mvc_input_model_dto/LoginDto.java
+++ src/main/java/com/virtualwallet/models/mvc_input_model_dto/LoginDto.java
@@ -2,14 +2,17 @@ package com.virtualwallet.models.mvc_input_model_dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import static com.virtualwallet.model_helpers.ModelConstantHelper.PASSWORD_ERROR_MESSAGE;
 import static com.virtualwallet.model_helpers.ModelConstantHelper.USERNAME_ERROR_MESSAGE;
 
 public class LoginDto {
     @NotEmpty(message = "Username can't be empty.")
+    @Size(min = 2, max = 20, message = "Username must be between 2 and 20 characters.")
     private String username;
     @NotEmpty(message = "Password can't be empty.")
+    @Size(min = 8, max = 65 ,message = "Password must be at least 8 characters long.")
     private String password;
 
     public String getUsername() {
