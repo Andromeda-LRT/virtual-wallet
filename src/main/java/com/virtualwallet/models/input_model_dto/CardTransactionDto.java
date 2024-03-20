2fe841de6ae2ed424cc39d856dbd93ff2304183e
TeodorK0907 <ted_kehayov@abv.bg>
2024-03-20 12:13:23 +0200

    Remove commented code and unused imports in input_model_dto

--- src/main/java/com/virtualwallet/models/input_model_dto/CardTransactionDto.java
+++ src/main/java/com/virtualwallet/models/input_model_dto/CardTransactionDto.java
@@ -2,9 +2,7 @@ package com.virtualwallet.models.input_model_dto;
 
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
 import org.checkerframework.checker.index.qual.Positive;
-import org.springframework.beans.factory.annotation.Value;
 
 public class CardTransactionDto {
     @Min(value = 5, message = "Amount must be at least 5.0 in order to make a transaction.")
