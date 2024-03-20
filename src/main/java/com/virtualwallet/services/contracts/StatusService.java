092d312e89f83233ced4cdf7832ae82e89933eef
TeodorK0907 <ted_kehayov@abv.bg>
2024-03-20 12:27:43 +0200

    Remove commented code and unused imports in services_contracts

--- src/main/java/com/virtualwallet/services/contracts/StatusService.java
+++ src/main/java/com/virtualwallet/services/contracts/StatusService.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface StatusService {
     Status createStatus(User user, Status status);
+
     void deleteStatus(int status_id, User user);
+
     void updateStatus(Status status, User user);
+
     Status getStatus(int status_id);
+
     List<Status> getAllStatuses();
 }
--- src/main/java/com/virtualwallet/services/contracts/WalletTransactionService.java
+++ src/main/java/com/virtualwallet/services/contracts/WalletTransactionService.java
@@ -7,14 +7,21 @@ import java.util.List;
 
 public interface WalletTransactionService {
     List<WalletToWalletTransaction> getAllWalletTransactions();
+
     List<WalletToWalletTransaction> getAllWalletTransactionsWithFilter
             (User user, WalletTransactionModelFilterOptions transactionFilter);
-    List<WalletToWalletTransaction> getUserWalletTransactions(User user, WalletTransactionModelFilterOptions transaction,
+
+    List<WalletToWalletTransaction> getUserWalletTransactions(User user,
+                                                              WalletTransactionModelFilterOptions transaction,
                                                               int wallet_id);
+
     WalletToWalletTransaction getWalletTransactionById(int walletTransactionId);
+
     boolean createWalletTransaction(User user, WalletToWalletTransaction transaction,
                                     Wallet senderWallet, Wallet recipientWallet);
+
     void approveTransaction(WalletToWalletTransaction transaction, Wallet recipientWallet);
+
     void cancelTransaction(WalletToWalletTransaction transaction);
 
 }
